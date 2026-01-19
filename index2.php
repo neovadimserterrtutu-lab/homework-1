26d03d78047a24d698b3c616f8dec4fadafac591
neovadimserterrtutu-lab <neo.vadimserterrtutu@gmail.com>
2026-01-19 14:57:15 +0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -7,19 +7,24 @@ $variable = 3.14;
 // $variable = null;
 // $variable = [];
 
-// Вариант 1: if / elseif
-if (is_bool($variable)) {
-    $type = 'bool';
-} elseif (is_float($variable)) {
-    $type = 'float';
-} elseif (is_int($variable)) {
-    $type = 'int';
-} elseif (is_string($variable)) {
-    $type = 'string';
-} elseif (is_null($variable)) {
-    $type = 'null';
-} else {
-    $type = 'other';
+switch (true) {
+    case is_bool($variable):
+        $type = 'bool';
+        break;
+    case is_float($variable):
+        $type = 'float';
+        break;
+    case is_int($variable):
+        $type = 'int';
+        break;
+    case is_string($variable):
+        $type = 'string';
+        break;
+    case is_null($variable):
+        $type = 'null';
+        break;
+    default:
+        $type = 'other';
 }
 
 echo "type is $type";
