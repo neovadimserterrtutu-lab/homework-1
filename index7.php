4805bcab4dbbabcc9b7ce5eb06fb1317f7e8eba4
neovadimserterrtutu-lab <neo.vadimserterrtutu@gmail.com>
2026-02-07 10:15:41 +0300

    Create index7.php

---
+++ index7.php
@@ -0,0 +1,21 @@
+<?php
+// PHP здесь можно вообще не писать, достаточно HTML,
+// но пусть будет корректный .php-файл
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Список страниц</title>
+</head>
+<body>
+    <h1>Домашнее задание: HTML-страницы</h1>
+
+    <ul>
+        <li><a href="notfound.php">1. Страница с кодом 404 (не найдена)</a></li>
+        <li><a href="download.php?text=Привет,+мир!">2. Скачивание txt-файла</a></li>
+        <li><a href="page3.php">3. Страница со счётчиком и редиректом</a></li>
+        <li><a href="page4.php">4. Страница с количеством открытий третьей</a></li>
+    </ul>
+</body>
+</html>
